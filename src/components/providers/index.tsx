3cee2571fe88ab56df6378009f047266951789ce
Jakub Godlewski <jakubgodlewski50@gmail.com>
2024-07-01 19:58:17 +0200

    add global styles, styles libraries etc

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,10 +1,14 @@
 import {PropsWithChildren} from "react";
+import {NextUIProvider} from "@nextui-org/react";
+import {useNavigate} from "react-router-dom";
 
 const Providers = ({children}: PropsWithChildren) => {
+    const navigate = useNavigate();
+
     return (
-        <div>
+        <NextUIProvider navigate={navigate}>
             {children}
-        </div>
+        </NextUIProvider>
     );
 };
 
